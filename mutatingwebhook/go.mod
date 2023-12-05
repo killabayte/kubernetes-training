ba8e7f1eb6fd532228859f0367349b1e130a34da
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-12-05 15:53:01 +0100

    k8s: Mutating webhook

--- mutatingwebhook/go.mod
+++ mutatingwebhook/go.mod
@@ -1,3 +1,3 @@
-module github.com:killabayte/kubernetes-training/mutatingwebhook
+module github.com/killabayte/kubernetes-training/mutatingwebhook
 
 go 1.21
--- mutatingwebhook/main.go
+++ mutatingwebhook/main.go
@@ -1,28 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
 	fmt.Printf("Initializing certificates...\n")
 	serverTLSConf, clientTLSConf, caPEM, err := certsetup()
+
 	if err != nil {
 		panic(err)
 	}
-}
 
-s := Server{
-	ServerTLSConf: serverTLSConf,
-	ClientTLSConf: clientTLSConf,
-	CaPEM: caPEM,
-}
-
-go func(){
-	handler := http.NewServeMux()
-
-	handler.HandleFunc("/ca.pem", s.getCA)
-	fmt.Printf("Starintg localhost http server on :8080 with ca.pem endpoint\n")
-	err = http.ListenAndServe("localhost:8080", handler)
-	if err != nil{
-		log.Fatal(err)
+	s := Server{
+		ServerTLSConf: serverTLSConf,
+		ClientTLSConf: clientTLSConf,
+		CaPEM:         caPEM,
 	}
-}()
+
+	go func() {
+		handler := http.NewServeMux()
+
+		handler.HandleFunc("/ca.pem", s.getCA)
+		fmt.Printf("Starintg localhost http server on :8080 with ca.pem endpoint\n")
+		err = http.ListenAndServe("localhost:8080", handler)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+}
