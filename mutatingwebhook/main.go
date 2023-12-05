0b3338f13ae813ebbdbb37754a8a8b8e6595a1c9
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-12-05 15:51:08 +0100

    k8s: Mutating webhook

--- mutatingwebhook/main.go
+++ mutatingwebhook/main.go
@@ -20,5 +20,6 @@ go func(){
 	handler := http.NewServeMux()
 
 	handler.HandleFunc("/ca.pem", s.getCA)
-	
+	fmt.Printf("Starintg localhost http server on :8080 with ca.pem endpoint\n")
+	err = http.ListenAndServe("localhost:8080", handler)
 }
